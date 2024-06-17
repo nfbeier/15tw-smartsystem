f90c696858f2d2b66875a1fbe3225de37c0cfe15
Nick Beier <nfbeier@gmail.com>
2024-06-17 09:29:11 -0600

    Added pathing check for XGS and Picomotor GUIs
    Replicated the pathing Liam introduced into the XPS GUI as the default method for pathing for each device GUI.

--- devices/NewportPicomotor/picomotorTest_Widget.py
+++ devices/NewportPicomotor/picomotorTest_Widget.py
@@ -3,7 +3,17 @@ from pylablib.devices import Newport
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-sys.path.append(r'C:\Users\HK-47\Documents\CODE\GitHub\15tw-smartsystem\devices\NewportPicomotor')
+cwd = os.getcwd()
+print(cwd)
+# Check if '15tw-smartsystem' is in the components
+if '15tw-smartsystem' not in cwd.split(os.path.sep):
+    raise ValueError("The directory does not contain '15tw-smartsystem' folder.")
+
+# Rebuild the directory string up to and including '15tw-smartsystem', prevent import errors
+cwd = os.path.sep.join(cwd.split(os.path.sep)[:cwd.split(os.path.sep).index('15tw-smartsystem') + 1])
+
+sys.path.insert(0,cwd)
+
 from picoWidgetTest_GUI import Ui_Form
 
 class picoMotor_App(QtWidgets.QWidget):
