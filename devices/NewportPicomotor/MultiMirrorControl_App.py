821986ad0b9b9215a43fc450ddcf8bd8ae0daa9d
Hakeem098 <124193929+Hakeem098@users.noreply.github.com>
2024-07-31 11:16:35 -0600

    Picomotor Fixes.

--- devices/NewportPicomotor/MultiMirrorControl_App.py
+++ devices/NewportPicomotor/MultiMirrorControl_App.py
@@ -6,7 +6,29 @@ from MirrorControlWidget import MirrorControlWidget
 # Create the main application to use the widget (Multi-mirror control GUI)
 
 class MultiMirrorControl_App(QtWidgets.QWidget):
+    '''
+    A QWidget subclass for controlling multiple mirrors using Newport Picomotor8742 stages.
+
+    Attributes:
+    stage1 : Newport.Picomotor8742 or None
+        The Picomotor stage controller for the first set of mirrors (2 mirrors specifically).
+    stage2 : Newport.Picomotor8742 or None
+        The Picomotor stage controller for the second set of mirrors.
+    mirror1 : MirrorControlWidget
+        Control widget for the first mirror.
+    mirror2 : MirrorControlWidget
+        Control widget for the second mirror.
+    mirror3 : MirrorControlWidget
+        Control widget for the third mirror.
+
+    Methods:
+    closeEvent(event)
+        Closes the stage controllers when the application is closed.
+    '''
     def __init__(self):
+        '''
+        Initializes the MultiMirrorControl_App with the specified Picomotor stage controllers and the control widgets.
+        '''
         super(MultiMirrorControl_App, self).__init__()
 
         try:
@@ -25,7 +47,7 @@ class MultiMirrorControl_App(QtWidgets.QWidget):
 
         print(f"Number of connected devices: {Newport.get_usb_devices_number_picomotor()}")
 
-        # Create widgets for each mirror mount
+        # Create widgets for each mirror 
         self.mirror1 = MirrorControlWidget(self.stage1, 1, 2, "Mirror 1")
         self.mirror2 = MirrorControlWidget(self.stage1, 3, 4, "Mirror 2")
         self.mirror3 = MirrorControlWidget(self.stage2, 1, 2, "Mirror 3")
@@ -41,6 +63,13 @@ class MultiMirrorControl_App(QtWidgets.QWidget):
         self.setFixedSize(1000, 400)
 
     def closeEvent(self, event):
+        '''
+        Closes the stage controllers when the application is closed.
+
+        Argument:
+        event : QCloseEvent
+            The close event that is triggered when the application window is closed.
+        '''
         if self.stage1:
             self.stage1.close()
         if self.stage2:
