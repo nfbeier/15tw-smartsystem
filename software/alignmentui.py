f6a736cbbd560564321b624b73bbc656b79989b0
Hakeem098 <124193929+Hakeem098@users.noreply.github.com>
2024-08-28 11:22:19 -0600

    create alignmentui.py
    Copied code from Liam's old branch and updated with Hakeem's correct mirror control widget

---
+++ software/alignmentui.py
@@ -0,0 +1,35 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout
+import sys, os
+
+cwd = os.getcwd()
+if '15tw-smartsystem' not in cwd.split(os.path.sep):
+    raise ValueError("The directory does not contain '15tw-smartsystem' folder.")
+# Rebuild the directory string up to and including '15tw-smartsystem', prevent import errors
+cwd = os.path.sep.join(cwd.split(os.path.sep)[:cwd.split(os.path.sep).index('15tw-smartsystem') + 1])
+sys.path.insert(0,cwd)
+
+from devices.Iris.irisgui import IrisGUIWidget
+from devices.NewportPicomotor.MultiMirrorControl_App import MultiMirrorControl_App
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    main_window = QWidget()
+    main_window.resize(800, 600)  # Set the default window size
+    main_window.setWindowTitle("Alignment-inator 9000")
+
+    layout = QVBoxLayout(main_window)
+    grid_layout = QGridLayout()
+    iris_gui = IrisGUIWidget()
+    grid_layout.addWidget(iris_gui, 0, 0)
+
+    pico_gui = MultiMirrorControl_App()
+    grid_layout.addWidget(pico_gui, 1, 0)
+
+    layout.addLayout(grid_layout)
+
+    main_window.setLayout(layout)
+
+    main_window.show()
+
+    sys.exit(app.exec_())
